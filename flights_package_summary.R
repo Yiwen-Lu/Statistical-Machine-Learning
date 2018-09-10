39ce17591ec8153b1791bcc8fc785674f6c37aff
Yiwen <35602732+Yiwen-Lu@users.noreply.github.com>
2018-09-10 19:22:52 +0200

    Add files via upload

---
+++ flights_package_summary.R
@@ -0,0 +1,23 @@
+# clear memory:
+rm(list = ls())
+
+# install package:
+install.packages("nycflights13")
+
+# load package:
+library(nycflights13)
+
+# load data:
+data(flights)
+View(flights)
+
+# max 'air_time':
+max_air_time <- max(flights$air_time, na.rm = TRUE) # remove the N/A!!!
+print(max_air_time)
+
+# mean of 'distance':
+mean_dist <- mean(flights$distance, na.rm = TRUE)
+print(mean_dist)
+
+# summary:
+print(summary(flights))
