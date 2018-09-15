9a611ed2bd200c94b82e2687a83ed56d9a48eed7
Yiwen <35602732+Yiwen-Lu@users.noreply.github.com>
2018-09-15 17:11:49 +0200

    Update kNN.R

--- kNN.R
+++ kNN.R
@@ -1,16 +1,16 @@
 rm(list = ls())
 
-work_dir <- "./RT2_kNN"
+work_dir <- "/home/lu/Documents/Vorlesung/SoSe2018/MLinR/RT2_kNN"
 setwd(work_dir)
 set.seed(42)
 
-dist <- function(a, b, norm = 1) {
+distance <- function(a, b, norm = 1) {
   if (norm == 1) {
     return(abs(a - b))
   } else if (norm == 2) {
     return(sqrt(sum((a-b)^2)))
   } else {
-    print("sorry, do not support other norms.")
+    stop("only supports Manhattan & Euclidean distance")
   }
 }
 
@@ -19,14 +19,14 @@ KNN_prediction <- function(x, y, K, x0) {
   # initialize prediction:
   pred <- data.frame(x0 = x0, y0 = NA)
   for (i in 1:n0) {
-    dist_x0i <- dist(x, x0[i])
-    rk <- rank(dist_x0i, ties.method = "random")
-    idx_nn <- which(rk <= K)  # get indices of NNs
+    dist <- distance(x, x0[i])
+    rk <- rank(dist, ties.method = "random")
+    nn_idx <- which(rk <= K)  # get indices of NNs
     "
     prediction is Pr(y = 1), if y is binary, i.e. {0,1};
     prediction is the mean,  if y is continuous.
     "
-    pred$y0[i] <- mean(y[idx_nn])
+    pred$y0[i] <- mean(y[nn_idx])
   }
   return(pred)
 }
